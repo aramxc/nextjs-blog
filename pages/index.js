dd9e62ad121845d6260b2c9c13546cf1c83a088a
Aaron Ramirez <aaronramirez@Aarons-MacBook-Pro.local>
2021-06-06 16:51:17 -0600

    implemented getStaticProps and getStaticPaths

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Aaron! At least, that was the name given to me at birth. Lately I've been going by A.J. Adopting a different name has helped me embrace a more fluid personality and let go of behaviors and patterns that no longer serve me as an adult</p>
+        <p>Hello, I'm Aaron!</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
